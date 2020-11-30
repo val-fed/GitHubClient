f308a5a7cdcdb7512eede394bc6164fb89411e83
val-fed <valerii.fedorenko.ua@gmail.com>
2020-11-30 11:41:19 +0200

    Add RepositoryAdapter

--- app/src/main/java/com/valfed/githubclient/MainActivity.java
+++ app/src/main/java/com/valfed/githubclient/MainActivity.java
@@ -2,11 +2,13 @@ package com.valfed.githubclient;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.valfed.githubclient.adapter.RepositoryAdapter;
 import com.valfed.githubclient.entity.Repository;
 import com.valfed.githubclient.network.HttpClient;
 
@@ -19,14 +21,19 @@ public class MainActivity extends AppCompatActivity {
   private static final String LOG_TAG = "dc.MainActivity";
 
   private HttpClient httpClient;
+  private RepositoryAdapter repositoryAdapter;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    httpClient = new HttpClient();
+    RecyclerView recyclerView = findViewById(R.id.repository_recycler_view);
+    recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    repositoryAdapter = new RepositoryAdapter();
+    recyclerView.setAdapter(repositoryAdapter);
 
+    httpClient = new HttpClient();
     new GetRepositoriesTask().execute("android");
   }
 
@@ -45,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPostExecute(List<Repository> result) {
       if (result != null) {
-        Log.d(LOG_TAG, result.toString());
+        repositoryAdapter.addItems(result);
       } else {
         Toast
             .makeText(MainActivity.this, R.string.error_message, Toast.LENGTH_SHORT)
---
+++ app/src/main/java/com/valfed/githubclient/adapter/RepositoryAdapter.java
@@ -0,0 +1,70 @@
+package com.valfed.githubclient.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+import com.valfed.githubclient.R;
+import com.valfed.githubclient.entity.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RepositoryAdapter extends RecyclerView.Adapter<RepositoryAdapter.RepositoryViewHolder> {
+
+  private final List<Repository> repositories = new ArrayList<>();
+
+  @NonNull
+  @Override
+  public RepositoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    Context context = parent.getContext();
+    LayoutInflater inflater = LayoutInflater.from(context);
+    View itemView = inflater.inflate(R.layout.repository_item_view, parent, false);
+    return new RepositoryViewHolder(itemView);
+  }
+
+  @Override
+  public void onBindViewHolder(@NonNull RepositoryViewHolder holder, int position) {
+    Repository repository = repositories.get(position);
+    holder.bind(repository);
+  }
+
+  @Override
+  public int getItemCount() {
+    return repositories.size();
+  }
+
+  public void addItems(List<Repository> items) {
+    repositories.addAll(items);
+    notifyDataSetChanged();
+  }
+
+  class RepositoryViewHolder extends RecyclerView.ViewHolder {
+    private final TextView nameTextView;
+    private final TextView descriptionTextView;
+    private final ImageView ownerImageView;
+
+    public RepositoryViewHolder(@NonNull View itemView) {
+      super(itemView);
+
+      nameTextView = itemView.findViewById(R.id.name_text_view);
+      descriptionTextView = itemView.findViewById(R.id.description_text_view);
+      ownerImageView = itemView.findViewById(R.id.owner_image_view);
+    }
+
+    void bind(Repository repository) {
+      nameTextView.setText(repository.getName());
+      descriptionTextView.setText(repository.getDescription());
+
+      String avatarUrl = repository.getOwner().getAvatarUrl();
+      Picasso.get().load(avatarUrl).into(ownerImageView);
+    }
+  }
+}
